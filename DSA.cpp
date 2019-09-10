ba9324033bfd67509975694b98fc0be1cd3f3ab3
alienx <me@alienx.cn>
2019-09-10 21:49:15 +0800

    Update DSA test

--- DSA.cpp
+++ DSA.cpp
@@ -16,7 +16,7 @@ int dsa_testp(void)
    dsa_key key, key2;
    prng_state prng;
 
-   ltc_mp = tfm_desc;
+   // ltc_mp = tfm_desc;
 
    if (ltc_mp.name == NULL) {
       printf("ERROR: ltc_mp.name == NULL!\n");
@@ -116,6 +116,132 @@ int dsa_testp(void)
    }
    dsa_free(&key2);
    dsa_free(&key);
+   printf("\n\n");
 
    return 0;
+}
+
+int DSA_generator_from_pqg(const char* g, const char* p, const char* q, const char* x, const char* y, dsa_key* prikey, dsa_key* pubkey){
+   unsigned char key_parts[5][256];
+   unsigned long key_lens[5];
+   // unsigned long len;
+   // unsigned char buf[1024];
+   const char* paras[5] = {p, q, g, y, x};
+
+   for (int i = 0; i < 5; i++){
+      key_lens[i] = sizeof(key_parts[i]);
+      radix_to_bin(paras[i], 16, key_parts[i], &key_lens[i]);
+   }
+   dsa_set_pqg(key_parts[0], key_lens[0],
+               key_parts[1], key_lens[1],
+               key_parts[2], key_lens[2],
+               prikey);
+   dsa_set_pqg(key_parts[0], key_lens[0],
+               key_parts[1], key_lens[1],
+               key_parts[2], key_lens[2],
+               pubkey);
+   dsa_set_key(key_parts[4], key_lens[4],
+               PK_PRIVATE, 
+               prikey);
+   dsa_set_key(key_parts[3], key_lens[3],
+               PK_PUBLIC, 
+               pubkey);
+   /*
+   len = sizeof(buf);
+   dsa_export(buf, &len, PK_PRIVATE | PK_STD, prikey);
+   if(compare_testvector(buf, len, openssl_priv_dsa, sizeof(openssl_priv_dsa), "what", __LINE__) == 0){
+      printf("Pri Com Successed!\n");
+   } else {
+      printf("Pri Com Failed!\n");
+   }
+
+   len = sizeof(buf);
+   dsa_export(buf, &len, PK_PUBLIC | PK_STD, pubkey);
+   if(compare_testvector(buf, len, openssl_pub_dsa, sizeof(openssl_pub_dsa), "what", __LINE__) == 0){
+      printf("Pub Com Successed!\n");
+   } else {
+      printf("Pub Com Failed!\n");
+   }
+   */
+   printf("DSA_generator_from_pqg:  OK!\n");
+   return CRYPT_OK;
+}
+
+int DSA_generator_from_pqg_random(dsa_key* prikey){
+   prng_state prng;
+   int stat = 0, err = 0;
+
+   /* register yarrow */
+   if (register_prng(&yarrow_desc) == -1) {
+      printf("Error registering Yarrow\n");
+      return -1;
+   }
+   /* setup the PRNG */
+   if ((err = rng_make_prng(128, find_prng("yarrow"), &prng, NULL)) != CRYPT_OK) {
+      printf("Error setting up PRNG, %s\n", error_to_string(err));
+      return -1;
+   }
+
+   dsa_generate_pqg(&prng, find_prng("yarrow"), 20, 128, prikey);
+   dsa_generate_key(&prng, find_prng("yarrow"), prikey);
+
+   dsa_verify_key(prikey, &stat);
+   if (stat == 0) {
+      printf("DSA_generator_from_pqg_random:  Failed!\n");
+      return CRYPT_FAIL_TESTVECTOR;
+   } else {
+      printf("DSA_generator_from_pqg_random:  OK!\n");
+      return CRYPT_OK;
+   }
+}
+
+int DSA_encrypt_message(const unsigned char* msg, unsigned long msg_len, unsigned char* out, unsigned long* out_len, const dsa_key* prikey){
+   prng_state prng;
+   int err = 0;
+
+   /* register hashes .... */
+   if (register_hash(&sha1_desc) == -1) {
+      printf("Error registering SHA1.\n");
+      return -1;
+   }
+   /* register yarrow */
+   if (register_prng(&yarrow_desc) == -1) {
+      printf("Error registering Yarrow\n");
+      return -1;
+   }
+   /* setup the PRNG */
+   if ((err = rng_make_prng(128, find_prng("yarrow"), &prng, NULL)) != CRYPT_OK) {
+      printf("Error setting up PRNG, %s\n", error_to_string(err));
+      return -1;
+   }
+
+   dsa_encrypt_key(msg, msg_len, out, out_len, &prng, find_prng("yarrow"), find_hash("sha1"), prikey);
+   return CRYPT_OK;
+}
+
+int DSA_decrypt_messgae(const unsigned char* in, unsigned long in_len, unsigned char* out, unsigned long* out_len, const dsa_key* prikey){
+   return dsa_decrypt_key(in, in_len, out, out_len, prikey);
+}
+
+int DSA_sign_hash(const unsigned char* in, unsigned long in_len, unsigned char* out, unsigned long* out_len, const dsa_key* prikey){
+   prng_state prng;
+   int err = 0;
+
+   /* register yarrow */
+   if (register_prng(&yarrow_desc) == -1) {
+      printf("Error registering Yarrow\n");
+      return -1;
+   }
+   /* setup the PRNG */
+   if ((err = rng_make_prng(128, find_prng("yarrow"), &prng, NULL)) != CRYPT_OK) {
+      printf("Error setting up PRNG, %s\n", error_to_string(err));
+      return -1;
+   }
+
+   dsa_sign_hash(in, in_len, out, out_len, &prng, find_prng("yarrow"), prikey);
+   return CRYPT_OK;
+}
+
+int DSA_sign_verify(const unsigned char* sig, unsigned long sig_len, const unsigned char* hash, unsigned long hash_len, int* stat, const dsa_key* prikey){
+   return dsa_verify_hash(sig, sig_len, hash, hash_len, stat, prikey);
 }
--- Test.cpp
+++ Test.cpp
@@ -1,5 +1,6 @@
 //
 // Created by Liming Shao on 2018/4/17.
+// Modified by alienx on 2019/9/9.
 //
 
 #include "Test.h"
@@ -17,7 +18,44 @@ int AES_Test() {
 
 int DSA_Test(){
     printf("DSA Test:\n");
-    dsa_testp();
+    ltc_mp = tfm_desc;
+    // dsa_testp();
+    // Test: 通过p、q、g参数来生成DSA密钥。
+    dsa_key prikey, pubkey;
+    const char *hex_g = "3B92E4FF5929150B08995A7BF2AD1440556FA047FF9099B344B3D4FC451505AE6722439CBA3710A5894737ECCCF5AEADA8B47A35CB9D935CEDE6B07E9694C4A60C7DD6708A094F814A0EC213FBEB16BFEAA4F456FF723005DE8A443FBEC6852655D62D1D1EDB15DAA445833C1797980B8D87F3490D90BDA9AB676E87687223DC";
+    const char *hex_p = "C50A37515CABD618D5A270BD4A6F6B4AF9E139950F2B99387D9A64D64CB5967ADCEDACA8ACC61B655ADEDB0061251A182CEEA10790625E4D123190C70321FA09E7B173D78EAFDBFDBFB3EFADD1A12A036DE706924A852AFF7A0166531FEAC66741845AC06CED62F9C2626205A4FA48A066EC35C9A811FEB981ABEEBE31B6BFCF";
+    const char *hex_q = "AA5BD7F4E5062413E58835CA00C7A635716194C5";
+    const char *hex_x = "9936E5E4E9FB28BE91F5065FE8C935B3F5D81FC5";
+    const char *hex_y = "5316B0FBBF598A5E5595C14FAC43B80853E6CF0D9223FAB184595239BFCBF22D383ADD935205497E2B12C46173E36F54BD96E5A7AAA95A58A4B767D2C0BDC81EB13A124F98C005EF395D6ABAB70B3BD8B795DD796EA2D28473470388B464D9B9B84FF1C934BBF97366F57C2E11FEC331E60838596781EB6D4127D70D74AFA035";
+    DSA_generator_from_pqg(hex_g, hex_p, hex_q, hex_x, hex_y, &prikey, &pubkey);
+    
+    // Test: 随机生成p、q、g参数来生成DSA密钥。
+    dsa_key prikey2;
+    DSA_generator_from_pqg_random(&prikey2);
+
+    // Test: DSA加解密。
+    unsigned char msg[16], out[1024], out2[1024];
+    unsigned long msg_len = sizeof(msg), x = sizeof(out), y = sizeof(out2);
+    int enc_code = DSA_encrypt_message(msg, msg_len, out, &x, &prikey2);
+    int dec_code = DSA_decrypt_messgae(out, x, out2, &y, &prikey2);
+    if(enc_code != 0 || dec_code != 0 || y != msg_len || memcmp(out2, msg, msg_len)){
+        printf("DSA Decrypt Failed!\n");
+    } else {
+        printf("DSA Decrypt Successed!\n");
+    }
+
+    // Test: DSA签名。
+    int stat1 = 0, stat2 = 0;
+    DSA_sign_hash(msg, msg_len, out, &x, &prikey2);
+    dsa_verify_hash(out, x, msg, msg_len, &stat1, &prikey2);
+    msg[0] ^= 1;
+    dsa_verify_hash(out, x, msg, msg_len, &stat2, &prikey2);
+    msg[0] ^= 1;
+    if (stat1 == 1 && stat2 == 0) {
+        printf("DSA Sign Successed!\n");
+    } else {
+        printf("DSA Sign Failed!\n");
+    }
     return 0;
 }
 
